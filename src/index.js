ddec2c7936f2737debdc4759feba9283c9cb4398
AmilAbdullazadeh <amilva@code.edu.az>
2019-12-31 14:23:43 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+toast.configure();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  const [product] = React.useState({
+    name: "Tesla Roadster",
+    price: 649555.68,
+    description: "Road car"
+  });
+
+  async function handleToken(token, addresses) {
+    const res = await axios.post(
+      "https://ry7v05l6on.sse.codesandbox.io/checkout",
+      { token, product }
+    );
+    const { status } = res.data;
+    console.log("Response: ", res.data);
+    if (status === "success") {
+      toast("Success! Check email for details", { type: "success" });
+    } else {
+      toast("Something went wrong", { type: "error" });
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="product">
+        <h1>{product.name}</h1>
+        <h3>On sale - ${product.price}</h3>
+      </div>
+      <StripeCheckout
+        token={handleToken}
+        stripeKey='pk_test_UN3KIGzjYGcke2sO4yomM6C800hzbfqmPd'
+        amount={product.price * 100}
+        name={product.name}
+        billingAddress
+        shippingAddress
+      />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
